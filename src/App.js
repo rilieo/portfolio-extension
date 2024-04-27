b0413a50b2b30c3227ddadd44720e2cd6b1e3a12
riley <rilesdou@gmail.com>
2024-04-26 23:57:35 -0400

    add base and components to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Hero from "./components/Hero";
+import Gallery from "./components/Gallery";
+import About from "./components/About";
+
+import { images } from "./data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero />
+      <About />
+      <Gallery children={images} />
     </div>
   );
 }
