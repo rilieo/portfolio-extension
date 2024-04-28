8e9af325af68bb1120838d51398d2e5ea279a440
riley <rilesdou@gmail.com>
2024-04-28 00:12:34 -0400

    adding data

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,29 @@
 import img1 from '../assets/gallery_images/IMG_6403.jpg';
 import img2 from '../assets/gallery_images/IMG_6449.jpg';
+import img3 from '../assets/gallery_images/IMG_2166.JPG';
 
-export const images = [img1, img2];
+const images = [
+    img1, 
+    img2, 
+    img3
+];
+
+var toDoItems = [
+    {
+        id: 1,
+        text: "Finish ML Project",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Study for tests",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Make Feature plushies",
+        completed: false
+    },
+];
+
+export { images, toDoItems };
